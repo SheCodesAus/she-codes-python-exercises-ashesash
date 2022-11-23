bc40cb486de10eabba90220a1e39eabaf189b655
ashesash <aishwarya.s@live.com>
2022-11-24 00:16:00 +1100

    oop lesson

--- oop/oop_playground.py
+++ oop/oop_playground.py
@@ -15,31 +15,35 @@ class Book:
 
     def turn_page(self):
         self.current_page += 1
+        return self.current_page
 
     def turn_page_back(self):
         self.current_page -= 1
+        return self.current_page
 
     def __str__(self) -> str:
         return f"The title of this book is {self.title} and the author is {self.author}"
 
-    def specific_page(self):
-        self.current_page = input("enter the page number ")
+    def go_specific_page(self):
+        self.specific_page = int(input("enter the page number "))
+        #using self.current_page does not work
 
     def bookmark(self):
         self.specific_page
 
     def start_again(self):
-        if self.current_page >= self.final_page:
-            self.current_page = 1
-
+        if self.specific_page >= self.final_page:
+            self.begin_page = 1
+            return self.begin_page
+        else:
+            return self.current_page
 
 
 my_book = Book("JK Rowling", "Harry Potter", 700)
-my_book.turn_page()
-
-print(my_book.title)
 print(my_book)
+print(my_book.turn_page())
 
-my_book.specific_page()
-print(f"the current page number is: {my_book.current_page}")
-print(f"final page:{my_book.final_page}")
+my_book.go_specific_page()
+# print(f"the current page number is: {my_book.current_page}")
+# print(f"final page: {my_book.final_page}")
+print(f"The book starts on page: {my_book.start_again()}")

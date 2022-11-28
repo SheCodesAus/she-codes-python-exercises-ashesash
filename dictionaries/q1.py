99cc2c7edc4bfdabc6e934ad469169b71c3775b6
ashesash <aishwarya.s@live.com>
2022-11-28 15:43:54 +1100

    solution to q1

--- dictionaries/q1.py
+++ dictionaries/q1.py
@@ -1,3 +1,13 @@
+import pprint
+
+### DICTIONARIES
+# dictionaries are mappings of key-value pairs
+# syntax:
+# my_dict = {
+#     'key1': 'value1',
+#     'key2' : 'value2'
+# }
+
 prices = {
     "Baby Spinach": 2.78,
     "Hot Chocolate": 3.70,
@@ -16,11 +26,28 @@ quantity = {
     "Oranges": 2
 }
 
-# quantity = {
-#     "Baby Spinach": 2,
-#     "Hot Chocolate": 1,
-#     "Crackers": 4,
-#     "Bacon": 0,
-#     "Carrots": 8,
-#     "Oranges": 5
-# }
+# print(list(i for i in prices)) 
+# for i in prices:
+#     prices_items = prices[i]
+#     print(f"{i}: ${prices_items}")
+# print("\n")
+
+# for item, price in prices.items():
+#     # print(f"{item}: ${price}")
+#     goods_prices = price
+
+# for item, quantities in quantity.items():
+#     goods_quantity = quantities
+
+# for items in prices.keys():
+#     print(f"{quantities} {items} @ ${price} = {price * quantities}")
+
+
+#dictionary comprehension
+# result = {key: round(prices[key] * quantity[key],2) for key in prices}
+# pprint.pprint(f"{result}")
+
+#method2
+for key in prices:
+    result = round(prices[key]*quantity[key],2)
+    print(f"{quantity[key]} {key} @ ${prices[key]} = ${result}")
